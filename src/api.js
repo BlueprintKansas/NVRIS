ad5ba5f1c75811fc6bed7418861d362c01540e20
Kunal Karnik <kunalkarnik@Kunals-MacBook-Pro.local>
2018-06-28 14:14:24 +0530

    Need Validation and Testing, but the English and Spanish VR forms now work. Also added snap of a test postman request.

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-const form = require('./imageManipulation').form
-import { VR } from './formDefinitions';
+import { VREN, VRES } from './formDefinitions';
 import { _ } from 'lodash';
 const app = express();
 const jsonParser = bodyParser.json();
@@ -10,25 +9,26 @@ var gm = require('gm').subClass({ imageMagick: true });
 require('gm-base64');
 
 app.get('/', (req, res) => {
-
   res.json({ message: 'ok' });
 });
 
 app.post('/vr/en', jsonParser, async (req, res) => {
   let form_payload = req.body;
-  //Kunal I think we can break out the form stuff into its own generator function?  maybe takes an form definition with url and the form payload?  that way we can reuse it on every endpoint?
-  let form = gm('https://s3.amazonaws.com/ksvotes/FEDVRENNVRIS.png').fontSize(40);
-  _.forEach(VR, await function(value, itr) {
+  let form = gm('https://s3.amazonaws.com/ksvotes/FEDVRENNVRIS.png');
+  _.forEach(VREN, await function(value, itr) {
     switch(value.type){
       case "draw":
         let fillText = form_payload[value.name];
         if(fillText){
-          form = form.drawText(value.x, value.y, form_payload[value.name])
+          form.fontSize(40);//To be set based on length(form_payload[value.name]), (x1,y1) and (x2,y2).
+          form = form.drawText(value.x1, value.y2, form_payload[value.name])
         }
         break;
       case "fill":
-        //needs rectangle boxes
-        break;
+      if(form_payload[value.name]){
+        form = form.drawRectangle(value.x1, value.y1, value.x2, value.y2)
+      }      
+      break;
     }
   });
 
@@ -36,15 +36,31 @@ app.post('/vr/en', jsonParser, async (req, res) => {
     let img_data = `data:image/gif;charset=utf-8;base64,${base64}`
     if(err) {console.log(err)};
     console.log(img_data);
-    // res.json({"img_data": img_data});
-    res.json({"hi":"WORLD"})
+    //res.json({"img_data": img_data});
+    res.json({"ENG":"form Generated"})
   });
 
 })
 
 app.post('/vr/es', jsonParser, async (req, res) => {
-  //https://s3.amazonaws.com/ksvotes/FEDVRSPANVRIS.png
-  res.json({message: "ok"})
+  let form_payload = req.body;
+  let form = gm('https://s3.amazonaws.com/ksvotes/FEDVRSPANVRIS.png');
+  _.forEach(VRES, await function(value, itr) {
+      let fillText = form_payload[value.name];
+      if(fillText){
+        form.fontSize(40);//To be set based on length(form_payload[value.name]), (x1,y1) and (x2,y2).
+        form = form.drawText(value.x1, value.y2, form_payload[value.name])
+      }
+  });
+
+  form = form.toBase64('gif', await function(err, base64){
+    let img_data = `data:image/gif;charset=utf-8;base64,${base64}`
+    if(err) {console.log(err)};
+    console.log(img_data);
+    //res.json({"img_data": img_data});
+    res.json({"SPA":"form Generated"})
+  });
+
 })
 
 app.post('/ab/en', jsonParser, async (req, res) => {
