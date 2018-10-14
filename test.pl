dd3411609aeacb94a0b9a3acd1b90a4fc4afb40f
Peter Karman <karpet@peknet.com>
2018-10-14 14:15:55 -0500

    Add actual tests, improve logging

---
+++ test.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+sub run_it {
+    my ($cmd) = @_;
+    print "$cmd\n";
+    system($cmd) and die "$cmd failed: $!";
+}
+
+my %tests = (
+    'test-vr-en-payload.json' => '/vr/en',
+    'test-ab-en-payload.json' => '/av/ksav1',
+);
+
+for my $payload ( sort keys %tests ) {
+    ( my $png = $payload ) =~ s/\.json/.png/;
+    my $path = $tests{$payload};
+    my $tmp  = "$payload-out.json";
+    run_it(
+        qq{curl -s -XPOST -H 'Content-Type: application/json' --data \@$payload http://localhost:4500$path > $tmp}
+    );
+    run_it(
+        qq{jq .img < $tmp | perl -n -e 's/"data:image\\/png;base64,|"//g; print' | base64 -D > $png}
+    );
+    print "PNG saved in $png\n";
+}
