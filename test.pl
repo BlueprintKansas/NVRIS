a43a8492be116e96a2a6c3f144ef54fce895687e
Peter Karman <karpet@peknet.com>
2018-10-15 22:43:49 -0500

    self-contained perl test script

--- test.pl
+++ test.pl
@@ -2,11 +2,38 @@
 
 use strict;
 use warnings;
+use JSON;
+use File::Slurper qw( read_text write_binary );
+use HTTP::Tiny;
+use MIME::Base64;
+use Data::Dump qw( dump );
 
 sub run_it {
     my ($cmd) = @_;
     print "$cmd\n";
     system($cmd) and die "$cmd failed: $!";
+}
+
+sub decode {
+    my ($str) = @_;
+    return MIME::Base64::decode_base64($str);
+}
+
+sub fetch_and_parse {
+    my ( $url, $payload, $outfile ) = @_;
+    my $agent = HTTP::Tiny->new();
+    my $resp  = $agent->request(
+        'POST', $url,
+        {   content => $payload,
+            headers => { 'Content-type' => 'application/json' }
+        }
+    );
+    die "$url failed: $resp->{reason}" unless $resp->{success};
+    my $body    = decode_json( $resp->{content} );
+    my $b64_img = $body->{img};
+    $b64_img =~ s/^data:image\/png;base64//g;
+    my $img = decode($b64_img);
+    write_binary( $outfile, $img );
 }
 
 my %tests = (
@@ -22,19 +49,16 @@ sub client_tests {
     for my $payload ( sort keys %tests ) {
         ( my $png = $payload ) =~ s/\.json/.png/;
         my $path = $tests{$payload};
-        my $tmp  = "$payload-out.json";
-        run_it(
-            qq{curl -s -XPOST -H 'Content-Type: application/json' --data \@$payload http://localhost:4501$path > $tmp}
-        );
-        run_it(
-            qq{jq .img < $tmp | perl -n -e 's/"data:image\\/png;base64,|"//g; print' | base64 $base64decode > $png}
-        );
-        print "PNG saved in $png\n";
+        fetch_and_parse( "http://localhost:4501$path", read_text($payload),
+            $png );
 
+        #print "PNG saved in $png\n";
         my @out = `file $png`;
-        print @out;
 
-        run_it("rm $tmp");
+        #print @out;
+        if ( grep {/PNG image data/} @out ) {
+            print "PASS\n";
+        }
     }
 
 }
@@ -43,11 +67,13 @@ my $pid = fork();
 
 if ($pid) {
     my $server_pid = fork() or exec("make run-test-server");
-    print "Master test process $$ waiting for server $server_pid and client $pid to finish\n";
-    waitpid($pid, 0);
+    print
+        "Master test process $$ waiting for server $server_pid and client $pid to finish\n";
+    waitpid( $pid, 0 );
     print "Child process finished\n";
     kill 'TERM', $server_pid;
     print "Sent TERM to $server_pid\n";
+    exit();
 }
 else {
     print "Waiting for server to start ...\n";
